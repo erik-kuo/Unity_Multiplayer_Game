248db31f97715c0a439a13cf7249916d25afc3a8
juliafu0409 <juliafu0409@gmail.com>
2021-01-08 22:14:05 +0800

    committt

--- Gameserver/Gameserver/Client.cs
+++ Gameserver/Gameserver/Client.cs
@@ -224,7 +224,7 @@ namespace GameServer
 				{
 					if (_client.id != id)
 					{
-						ServerSend.SpawnPlayer(id, _client.player);
+						ServerSend.SpawnPlayer(id, _client.player, Server.serverTimer.timeRemaining);
 					}
 				}
 			}
@@ -234,7 +234,7 @@ namespace GameServer
 			{
 				if (_client.player != null)
 				{
-					ServerSend.SpawnPlayer(_client.id, player);
+					ServerSend.SpawnPlayer(_client.id, player, Server.serverTimer.timeRemaining);
 				}
 			}
 		}
